52c35b177b66328b1dfd404b08799ec383b868bd
Louie Cabral <louiealdrin25@gmail.com>
2021-12-22 15:42:37 +0800

    Environment Done

---
+++ src/pages/Landing/Landing.js
@@ -0,0 +1,10 @@
+import StyleLanding from "./Landing.style";
+function Landing(){
+    return(
+      <StyleLanding>
+          <h1>Hello World</h1>
+      </StyleLanding>
+    )
+}
+
+export default Landing;
---
+++ src/pages/Landing/Landing.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+const StyleLanding = styled.div`
+height:500px;
+background-color:red;
+`
+export default StyleLanding;
